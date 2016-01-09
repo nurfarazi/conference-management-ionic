89480d4beb122587972e9d25fe15484a9afb0c11
farazi <farazi@Shumanas-MacBook-Pro.local>
2016-01-09 11:40:48 +0600

    working

--- www/js/controller.js
+++ www/js/controller.js
@@ -3,7 +3,19 @@ var app = angular.module('ionicApp');
 
 app.controller('SignInCtrl', function ($scope, $state, $http, $window) {
 
-    $state.go('home');
 
+
+    $scope.signIn = function () {
+        $state.go('home');
+    }
+
+    console.log('signin');
+});
+app.controller('homeCtrl', function ($scope, $state, $http, $window) {
+
+
+
+
+    console.log('home');
 
 });
